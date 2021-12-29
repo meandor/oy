9d5342c14226f17505f6423b3a51f0e128379ad5
Daniel Schruhl <daniel.schruhl@gropyus.com>
2021-12-29 19:44:35 +0100

    Add initial stuff

---
+++ backend/project.clj
@@ -0,0 +1,7 @@
+(defproject oy "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/meandor/oy"
+  :license {:name "MIT"
+            :url "https://github.com/meandor/oy/blob/main/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns app.core})
---
+++ backend/test/oy/core_test.clj
@@ -0,0 +1,7 @@
+(ns oy.core-test
+  (:require [clojure.test :refer :all]
+            [oy.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

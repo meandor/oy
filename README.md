26dffc1eeae95fb00ee12304ac437c481bca3ce1
Daniel Schruhl <daniel.schruhl@gropyus.com>
2021-12-29 19:37:50 +0100

    Add initial skeleton

---
+++ README.md
@@ -0,0 +1,6 @@
+# Miki
+A _Clojure_ app to give you a nice overview of your finances.
+
+## Usage
+
+FIXME
---
+++ src/oy/core.clj
@@ -0,0 +1,6 @@
+(ns oy.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
